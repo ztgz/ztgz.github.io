2372acc2d633ee443b8461c1e1a056e58287e288
Arvid Zetterberg <zetterberg.arvid@gmail.com>
2021-11-14 12:11:48 +0100

    #Appsettings set all colors

--- ArvidZetterberg/Store/SettingsStore.cs
+++ ArvidZetterberg/Store/SettingsStore.cs
@@ -22,5 +22,60 @@
                 InvokeUpdates();
             }
         }
+
+        private string windowFontColor = "#fffaf0";
+        public string WindowFontColor
+        {
+            get => windowFontColor;
+            set
+            {
+                windowFontColor = value;
+                InvokeUpdates();
+            }
+        }
+
+        private string windowContentBackgroundColor = "#FFFFFF";
+        public string WindowContentBackgroundColor
+        {
+            get => windowContentBackgroundColor;
+            set
+            {
+                windowContentBackgroundColor = value;
+                InvokeUpdates();
+            }
+        }
+
+        private string iconColor = "#000";
+        public string IconColor
+        {
+            get => iconColor;
+            set
+            {
+                iconColor = value;
+                InvokeUpdates();
+            }
+        }
+
+        private string textColor = "#000";
+        public string TextColor
+        {
+            get => textColor;
+            set
+            {
+                textColor = value;
+                InvokeUpdates();
+            }
+        }
+
+        private string toolbarColor = "#d3d3d3";
+        public string ToolbarColor
+        {
+            get => toolbarColor;
+            set
+            {
+                toolbarColor = value;
+                InvokeUpdates();
+            }
+        }
     }
 }
