b5f01b74d6be5d39df767b1140723236dc89b86b
Arvid Zetterberg <zetterberg.arvid@gmail.com>
2022-06-03 12:10:39 +0200

    Update publish batch file

--- publish.bat
+++ publish.bat
@@ -1,1 +1,19 @@
+@echo off
+
+for %%f in (.\docs\*) do (
+  call :deleteBlazorFiles %%f
+)
+
+for /d %%d in (.\docs\*) do del /Q %%d
+
+goto:Publish
+
+:deleteBlazorFiles
+  if %~1 == .\docs\.nojekyll EXIT /B 0
+  if %~1 == .\docs\CNAME EXIT /B 0
+  del %~1 
+EXIT /B 0
+
+:Publish  
+@echo on
 dotnet publish -r win-x64 --self-contained -p:PublishTrimmed=true -c Release -o docs
